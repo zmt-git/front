7d1bf8cd3ca98129d10f9ab855c65aadb1b934c9
ZMT <ZMT819@126.com>
2021-03-01 15:03:28 +0800

    feature service worker

---
+++ JavaScript/Service Worker  PWA/index.js
@@ -0,0 +1,14 @@
+/**
+ * ? Service Worker
+ * *Service Worker 是一个浏览器背后运行的脚步，独立于 web 页面，为无需一个页面或用户交互的功能打开了大门。
+ * *今日，它包含了推送通知和背景异步（push notifications and background sync）的功能。
+ * *将来，Service Worker 将支持包括 periodic sync or geofencing 的功能
+ * 
+ * 特点
+ * *Service Worker 在其自己的全局上下文中运行
+ * *它没有绑定到特定的网页
+ * *它不能访问到 DOM
+ * 
+ * ? PWA Progressive Web App
+ * 
+ */
---
+++ JavaScript/Service Worker  PWA/practice.js
@@ -0,0 +1,38 @@
+window.onload = function () {
+  if ('serviceWorker' in navigator) {
+    $('#isSupport').text('支持')
+    console.log('支持')
+  
+    navigator.serviceWorker.register('./index.js', { scope: './' })
+      .then(registration => {
+        $('#isSuccess').text('注册成功')
+  
+        let serviceWorker = null
+  
+        if (registration.installing) {
+          serviceWorker = registration.installing
+          $('#state').text('installing')
+        } else if (registration.waiting) {
+          serviceWorker = registration.waiting
+          $('#state').text('waiting')
+        } else if (registration.active) {
+          serviceWorker = registration.active
+          $('#state').text('active')
+        }
+  
+        if (serviceWorker) {
+          $('#swState').text(serviceWorker.state)
+          serviceWorker.addEventListener('statechange', (e) => {
+            $('#swState').append('&emsp;状态变化为' + e.target.state)
+          })
+        }
+      })
+      .catch(e => {
+        $('#isSuccess').text('注册失败')
+        console.log(e)
+      })
+  } else {
+    $('#isSupport').text('不支持')
+    console.log('不支持')
+  }
+}
