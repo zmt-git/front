5302ab5f06446c863841af89e4a7d3094b6cca74
ZMT <ZMT819@126.com>
2021-03-03 11:13:11 +0800

    feature 数组处理

---
+++ 3.Frame/1.MVVM/array.js
@@ -0,0 +1,19 @@
+// *改写数组属性
+const aryMethods = ['push', 'pop', 'shift', 'unshift', 'splice', 'sort', 'reverse'];
+const arrayAugmentations = [];
+
+aryMethods.forEach((method)=> {
+
+    // 这里是原生Array的原型方法
+    let original = Array.prototype[method];
+
+   // 将push, pop等封装好的方法定义在对象arrayAugmentations的属性上
+   // 注意：是属性而非原型属性
+    arrayAugmentations[method] = function () {
+        console.log('我被改变啦!');
+
+        // 调用对应的原生方法并返回结果
+        return original.apply(this, arguments);
+    };
+
+})
